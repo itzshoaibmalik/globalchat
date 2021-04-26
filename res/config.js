bdadf5be8ec7258a9d0c916bcad31dbfec7cec59
Shoaib Malik <70048082+itzshoaibmalik@users.noreply.github.com>
2021-04-26 20:12:29 +0530

    Create config.js

---
+++ res/config.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCuBzznka7Eb1dopxBDk8_WLKqa47vwwAw",
+    authDomain: "globalchatx.firebaseapp.com",
+    databaseURL: "https://globalchatx-default-rtdb.firebaseio.com",
+    projectId: "globalchatx",
+    storageBucket: "globalchatx.appspot.com",
+    messagingSenderId: "224091989513",
+    appId: "1:224091989513:web:f0fdfcb855c1f409ddfad8",
+    measurementId: "G-41CNJD1K5G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
